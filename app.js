10f3d1143eaef2049912b54fce1fdbadfc8c256e
davy2407 <54894522+davy2407@users.noreply.github.com>
2019-10-18 15:21:34 +0200

    function radius v2
    la fonction testlocalisation utilise fonction recupVille pour afficher les coord d'une ville et ensuite afficher les entreprises d'un secteur renseigné sur un rayon renseigné à l'avance

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
 function API(){
 	
 	
-	const recherche = fetch('https://entreprise.data.gouv.fr/api/sirene/v1/full_text/*?activite_principale=1071C&code_postal=76000&per_page=50').then( resultat => resultat.json()).then( json => json )
+	const recherche = fetch('https://entreprise.data.gouv.fr/api/sirene/v1/full_text/*?activite_principale=6201Z&code_postal=76000&per_page=50').then( resultat => resultat.json()).then( json => json )
 	console.log(recherche);
 }
 
 API();
 
 
-function testLocalisation(){
+async function testLocalisation(){
+    const ville = recupVille();
 
-    const loc = fetch('https://api-adresse.data.gouv.fr/search/?q=Montville').then( resultat => resultat.json()).then(json => json.features[0].geometry.coordinates[0])
-    return loc;    
+    let loc = await fetch('https://api-adresse.data.gouv.fr/search/?q='+ville).then( resultat => resultat.json()).then(json => json.features[0].geometry.coordinates)
+    
+    function radiusEntreprise(){
+        // trouve les entreprises dans un rayon de recherche
+        // lat/long parametre: radius 5km de base 
+        // affiche la deuxieme page 
+        // 10 resultats par pages par default (per_page = ) pour modifier
+        const radius = fetch('https://entreprise.data.gouv.fr/api/sirene/v1/near_point/?lat='+loc[1]+'&long='+loc[0]+'&activite_principale=6201Z&radius=20&page=1').then(resultat => resultat.json()).then(json => json)
+        console.log("affiche les entreprises de la ville demandée dans un rayon de 20 km ")
+        console.log(radius);
+    }
+    radiusEntreprise();
+   
 }
 
-var test = testLocalisation();
-
-console.log(test);
 
 
 
 
-function radiusEntreprise(){
-    // trouve les entreprises dans un rayon de recherche
-    // lat/long parametre: radius 5km de base 
-    // affiche la deuxieme page 
-    // 10 resultats par pages par default (per_page = ) pour modifier
-    const radius = fetch('https://entreprise.data.gouv.fr/api/sirene/v1/near_point/?lat=49.543856&long=1.082285&activite_principale=1071C&radius=5&page=2').then(resultat => resultat.json()).then(json => json.etablissements)
-    console.log(radius);
+
+
+
+
+
+
+
+
+
+///function recup la ville utilisateur
+
+
+function recupVille(){
+    const ville = document.getElementById('rechercheVille').value;
+    return ville;
 }
 
-radiusEntreprise();
+document.getElementById('boutton').addEventListener('click', testLocalisation);
+
+
 
